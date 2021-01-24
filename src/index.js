71736d05bac62cad2bf7edafc4bc25efcd37e76f
ananthk19 <ananthakrishnan2019@iiitkottayam.ac.in>
2021-01-24 17:57:33 +0530

    update 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
+import BookList from './components/components.BookList';
+import './components/styles/components.BookList.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*function Heading(){
+  return (
+    <React.Fragment>
+      <h1>Heading</h1>
+    </React.Fragment>
+  )
+}
+
+function Message(){
+  return (
+    <React.Fragment>
+      <h3><i>Message</i></h3>
+    </React.Fragment>
+  )
+}
+*/
+function Store(){
+  return (
+    <React.Fragment className='bookList'>
+      <BookList />
+      <BookList />
+      <BookList />
+    </React.Fragment>
+    
+  )
+}
+ReactDOM.render(<Store />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
